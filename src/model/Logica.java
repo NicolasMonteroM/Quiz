6cad425b5c66e13eb3eb8844c9d01bbcdd750cbc
Nicolas Montero <nicolasmontero42@gmail.com>
2020-10-10 09:06:42 -0500

    Creating classes and structure

---
+++ src/model/Logica.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Logica {
+
+	public Logica() {
+		
+	}
+	
+	public void drawLogica() {
+		
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,35 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+
+	}
+
+	@Override
+	public void settings() {
+		
+		size(600,600);
+	}
+	
+	@Override
+	public void setup() {
+		
+	}
+	
+	@Override
+	public void draw() {
+		
+		background(50);
+		
+	}
+	
+	@Override
+	public void mousePressed() {
+		
+
+	}
+}
